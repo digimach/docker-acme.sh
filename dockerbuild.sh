e5c9e8f74f9c494d00f53722355b4473451f7afc
Komail Kanjee <kkanjee@eeng.pro>
2021-02-21 22:20:15 -0500

    Handling acmesh version as part of the docker build depending on the ref

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -12,14 +12,17 @@ DOCKER_IMAGE="digimach/acme.sh"
 DOCKER_IMAGE_TAG="${BASE_IMAGE}-dev"
 GITHUB_REF=""
 AUTO_UPGRADE=0
+ACMESH_VERSION=master
 
 if [[ "$GITHUB_REF" == refs/tags/* ]]; then
-    DOCKER_IMAGE_TAG="${BASE_IMAGE}-${GITHUB_REF#refs/tags/}"
+    ACMESH_VERSION="${GITHUB_REF#refs/tags/}"
+    DOCKER_IMAGE_TAG="${BASE_IMAGE}-${ACMESH_VERSION}"
 elif [[ "$GITHUB_REF" == refs/heads/* ]]; then
-    DOCKER_IMAGE_TAG="${BASE_IMAGE}-${GITHUB_REF#refs/heads/}"
+    ACMESH_VERSION="${GITHUB_REF#refs/heads/}"
+    DOCKER_IMAGE_TAG="${BASE_IMAGE}-${ACMESH_VERSION}"
 fi
 
-if [[ "$DOCKER_IMAGE_TAG" == master ]]; then
+if [[ "$ACMESH_VERSION" == master ]]; then
     DOCKER_IMAGE_TAG="${BASE_IMAGE}-latest"
     AUTO_UPGRADE=1
 fi
@@ -29,5 +32,6 @@ cd "$BASE_IMAGE" || exit
 docker buildx build \
     --tag "$DOCKER_IMAGE:$DOCKER_IMAGE_TAG" \
     --output "$DOCKER_OUTPUT" \
-    --build-arg AUTO_UPGRADE="$AUTO_UPGRADE" \
+    --build-arg auto_upgrade="$AUTO_UPGRADE" \
+    --build-arg acmesh_version="$ACMESH_VERSION" \
     --platform "$SUPPORTED_ARCHITECTURES" .
