b50e85bed3a1594a9362d10435dfeae9ba16353e
Komail Kanjee <kkanjee@eeng.pro>
2021-02-24 10:13:42 -0500

    Adding debian base build scripts

---
+++ debian/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+if [[ $EUID -ne 0 ]]; then
+    echo "** Docker container is not started with root user. This is more secure way of handling this container and this message is only for information."
+fi
+
+if [[ "$1" == "_" ]]; then
+    shift
+    "$@"
+elif [[ -z "$1" ]]; then
+    acme.sh --help
+else
+    cd "$LE_WORKING_DIR" || exit
+    acme.sh "$@"
+fi
---
+++ debian/env.sh
@@ -0,0 +1,3 @@
+# shellcheck shell=bash
+export SUPPORTED_ARCHITECTURES="linux/386,linux/amd64,linux/arm64/v8,linux/ppc64le,linux/s390x"
+export UNSUPPORTED_ARCHITECTURES="linux/arm/v7"
