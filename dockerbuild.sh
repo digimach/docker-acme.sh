0d877895a0e6ea53ed3e102618dc43e78d5ad2be
Komail Kanjee <kkanjee@eeng.pro>
2021-02-23 21:16:22 -0500

    Publishing containers to local registry and only pushing on certain branches
    * Moving docker image tag logic to .github/workflows/docker_build.yaml
    * Remvoing AUTO_UPGRADE flag as it should not be set
    * Passing Docker image tag to dockerbuild.sh along with image name from .github/workflows/docker_build.yaml

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -6,32 +6,16 @@ BASE_IMAGE=$1
 
 source "$BASE_IMAGE/env.sh"
 
-DOCKER_OUTPUT="${DOCKER_OUTPUT:-type=image,push=true}"
+DOCKER_OUTPUT="${DOCKER_OUTPUT:-type=image}"
 SUPPORTED_ARCHITECTURES="${SUPPORTED_ARCHITECTURES}"
-DOCKER_IMAGE="digimach/acme.sh"
-DOCKER_IMAGE_TAG="${BASE_IMAGE}-dev"
-GITHUB_REF=""
-AUTO_UPGRADE=0
-ACMESH_VERSION=master
-
-if [[ "$GITHUB_REF" == refs/tags/* ]]; then
-    ACMESH_VERSION="${GITHUB_REF#refs/tags/}"
-    DOCKER_IMAGE_TAG="${BASE_IMAGE}-${ACMESH_VERSION}"
-elif [[ "$GITHUB_REF" == refs/heads/* ]]; then
-    ACMESH_VERSION="${GITHUB_REF#refs/heads/}"
-    DOCKER_IMAGE_TAG="${BASE_IMAGE}-${ACMESH_VERSION}"
-fi
-
-if [[ "$ACMESH_VERSION" == master ]]; then
-    DOCKER_IMAGE_TAG="${BASE_IMAGE}-latest"
-    AUTO_UPGRADE=1
-fi
+DOCKER_IMAGE="${DOCKER_IMAGE:-digimach/acme.sh}"
+DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG:-$BASE_IMAGE-dev}"
+ACMESH_VERSION="${ACMESH_VERSION:-master}"
 
 cd "$BASE_IMAGE" || exit
 
 docker buildx build \
     --tag "$DOCKER_IMAGE:$DOCKER_IMAGE_TAG" \
     --output "$DOCKER_OUTPUT" \
-    --build-arg auto_upgrade="$AUTO_UPGRADE" \
     --build-arg acmesh_version="$ACMESH_VERSION" \
     --platform "$SUPPORTED_ARCHITECTURES" .
