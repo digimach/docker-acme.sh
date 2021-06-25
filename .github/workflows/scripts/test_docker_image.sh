d1578b02614003d3a6510eecf3c1bd94aa728676
Komail Kanjee <kkanjee@eeng.pro>
2021-06-25 16:02:57 -0400

    bugfix in .github/workflows/scripts/test_docker_image.sh - handling expected error code

--- .github/workflows/scripts/test_docker_image.sh
+++ .github/workflows/scripts/test_docker_image.sh
@@ -11,4 +11,12 @@ docker run -t --env PUID=0 --env PGID=1008 "$DOCKER_IMAGE":"$DOCKER_IMAGE_TAG" i
 
 # Check locking mechanism
 mkdir -p /tmp/acmesh
+set +e
 flock --exclusive --nonblock --conflict-exit-code 11 --verbose /tmp/acmesh/run.lock docker run -t --volume /tmp/acmesh/:/acmesh/config "$DOCKER_IMAGE":"$DOCKER_IMAGE_TAG" acme.sh
+retVal=$?
+if [ $retVal -eq 11 ]; then
+    echo "---- Error code 11 was found as expected"
+else
+    echo "---- Error code 11 not found. Failing!"
+    exit 1
+fi
