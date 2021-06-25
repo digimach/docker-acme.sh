cdad065cdba83a06fbe3ca6704ce03715623abba
Komail Kanjee <kkanjee@eeng.pro>
2021-06-25 15:36:33 -0400

    Adding locking to process to ensure only single instance runs per config.
    * Modified acme.sh to make use of flock
    * Added test to check file lock function

--- ubuntu/root/usr/local/bin/acme.sh
+++ ubuntu/root/usr/local/bin/acme.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -ue
 
-exec s6-setuidgid "$PUID":"$PGID" "$LE_WORKING_DIR/acme.sh" "$@"
+LOCKFILE="$LE_CONFIG_HOME/run.lock"
+
+flock --exclusive --nonblock --conflict-exit-code 11 --verbose "$LOCKFILE" exec s6-setuidgid "$PUID":"$PGID" "$LE_WORKING_DIR/acme.sh" "$@"
