1543a5bfe0875d8da633723c1a06ca57ba235be5
Komail Kanjee <kkanjee@eeng.pro>
2021-06-25 16:13:32 -0400

    Handling flock for alpine that does not except args like other distros. Updating test as well

--- .github/workflows/scripts/test_docker_image.sh
+++ .github/workflows/scripts/test_docker_image.sh
@@ -14,9 +14,9 @@ mkdir -p /tmp/acmesh
 set +e
 flock --exclusive --nonblock --conflict-exit-code 11 --verbose /tmp/acmesh/run.lock docker run -t --volume /tmp/acmesh/:/acmesh/config "$DOCKER_IMAGE":"$DOCKER_IMAGE_TAG" acme.sh
 retVal=$?
-if [ $retVal -eq 11 ]; then
-    echo "---- Error code 11 was found as expected"
+if [ $retVal -ne 0 ]; then
+    echo "---- Error code was found as expected"
 else
-    echo "---- Error code 11 not found. Failing!"
+    echo "---- Error code not found. Failing!"
     exit 1
 fi
--- alpinelinux/root/usr/local/bin/acme.sh
+++ alpinelinux/root/usr/local/bin/acme.sh
@@ -2,4 +2,4 @@
 
 LOCKFILE="$LE_CONFIG_HOME/run.lock"
 
-flock --exclusive --nonblock --conflict-exit-code 11 --verbose "$LOCKFILE" exec s6-setuidgid "$PUID":"$PGID" "$LE_WORKING_DIR/acme.sh" "$@"
+flock -x -n "$LOCKFILE" exec s6-setuidgid "$PUID":"$PGID" "$LE_WORKING_DIR/acme.sh" "$@"
