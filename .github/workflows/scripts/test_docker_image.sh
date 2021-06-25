1769e8f46354b61a41710d01441038c78df175cc
Komail Kanjee <kkanjee@eeng.pro>
2021-06-25 15:52:40 -0400

    Bugfix in .github/workflows/scripts/test_docker_image.sh fixing image name and tag. Dropping interactive flag

--- .github/workflows/scripts/test_docker_image.sh
+++ .github/workflows/scripts/test_docker_image.sh
@@ -11,4 +11,4 @@ docker run -t --env PUID=0 --env PGID=1008 "$DOCKER_IMAGE":"$DOCKER_IMAGE_TAG" i
 
 # Check locking mechanism
 mkdir -p /tmp/acmesh
-flock --exclusive --nonblock --conflict-exit-code 11 --verbose /tmp/acmesh/run.lock docker run -ti --volume /tmp/acmesh/:/acmesh/config 8117466df461b7f01e825feb5d5383a2bd5a2b39b0e27baa949e7c85498403de acme-lock.sh
+flock --exclusive --nonblock --conflict-exit-code 11 --verbose /tmp/acmesh/run.lock docker run -t --volume /tmp/acmesh/:/acmesh/config "$DOCKER_IMAGE":"$DOCKER_IMAGE_TAG" acme.sh
