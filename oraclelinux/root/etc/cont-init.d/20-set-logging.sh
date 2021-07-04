a5cfbe01369fee20077b419e6e5718b278cf2fc7
Komail Kanjee <kkanjee@eeng.pro>
2021-07-04 17:41:40 -0400

    Fixing logging to run as the same user as the process

--- oraclelinux/root/etc/cont-init.d/20-set-logging.sh
+++ oraclelinux/root/etc/cont-init.d/20-set-logging.sh
@@ -11,10 +11,10 @@ if [ -v ACMESH_DAEMON ] && [[ $ACMESH_DAEMON == "1" ]]; then
     echo "** Setting up logging for daemon"
 
     if ! [ -v S6_LOGGING_SCRIPT ]; then
-        printf 'n30 s10000000 S15000000 T !"gzip -nq9"' >/var/run/s6/container_environment/S6_LOGGING_SCRIPT
+        printf "n30 s10000000 S15000000 T !'gzip -nq9'" >/var/run/s6/container_environment/S6_LOGGING_SCRIPT
     fi
 
     mkdir -p "$LE_LOG_DIR"
-    chown -R nobody "$LE_LOG_DIR"
+    chown -R "$PUID:$PGID" "$LE_LOG_DIR"
     chmod -R o+w "$LE_LOG_DIR"
 fi
