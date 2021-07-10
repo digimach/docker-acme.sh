aa7090ceeadb421b0e362dd1737704a67f54af6e
Komail Kanjee <kkanjee@eeng.pro>
2021-07-10 19:18:09 -0400

    Dropping oraclelinux disto until CVE is resolved

--- oraclelinux/root/usr/local/bin/acme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-
-LOCKFILE="$LE_CONFIG_HOME/run.lock"
-
-flock --exclusive --nonblock --conflict-exit-code 11 --verbose "$LOCKFILE" exec s6-setuidgid "$PUID":"$PGID" "$LE_WORKING_DIR/acme.sh" "$@"
